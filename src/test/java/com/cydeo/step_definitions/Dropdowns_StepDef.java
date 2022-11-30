82693b4d52ec9fad22e0fe8849b3a9a5cd6987d8
manizhazaki <manijezaki@gmail.com>
2022-11-29 19:02:15 -0600

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/Dropdowns_StepDef.java
@@ -0,0 +1,41 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.DropDownsPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dropdowns_StepDef {
+
+    DropDownsPage dropDownsPage = new DropDownsPage();
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown ");
+
+    }
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
+/*
+        Select select = new Select(dropDownsPage.monthDropdown);
+
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for(WebElement each : actualOptionsAsWebElement){
+            actualOptionsAsString.add(each.getText());
+        }
+
+ */
+        // this utility method will return us List of String of given dropdown WebElement
+        List<String> actualOptionsAsString = BrowserUtils.dropdownOptionsAsString(dropDownsPage.monthDropdown);
+        //assert checks the size of List first. if it is matching it will check/verify content 1 by 1
+        Assert.assertEquals(expectedMonths,actualOptionsAsString);
+    }
+}
